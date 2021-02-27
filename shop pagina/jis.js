b0f352e359b2666e94fba5d5793a5229a334f830
ThePipeBombBeast <kylianfrags@gmail.com>
2021-02-27 16:43:58 +0100

    d

--- shop pagina/jis.js
+++
@@ -1,8 +0,0 @@
-function clicky() {
-    var x = document.getElementById("loogin");
-    if (x.className.indexOf("logindown") == -1) {
-        x.className += " logindown";
-    } else {
-        x.className = x.className.replace(" logindown", "");
-    }
-}
